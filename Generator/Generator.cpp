48b09ad456114e5dd65d7fded3b45a2fd591332d
Galysso <jocelin.cailloux@gmail.com>
2017-02-03 14:13:47 +0100

    Générateur d'instances meilleur mais ecart-type faible

--- Generator/Generator.cpp
+++ Generator/Generator.cpp
@@ -69,25 +69,29 @@ void connect(int V1, int V2, int **graph) {
 }
 
 bool areConnected(int V1, int V2, int **graph) {
-	bool res = false;
-	int Vi, Vj, card, ind;
-	
-	if (graph[V2][0] < graph[V1][0]) {
-		card = graph[V2][0];
-		Vi = V2;
-		Vj = V1;
+	bool res;
+	if (V1 != V2) {
+		res = false;
+		int Vi, Vj, card, ind;
+		
+		if (graph[V2][0] < graph[V1][0]) {
+			card = graph[V2][0];
+			Vi = V2;
+			Vj = V1;
+		} else {
+			card = graph[V1][0];
+			Vi = V1;
+			Vj = V2;
+		}
+		
+		ind = 1;
+		while (!res && (ind <= card)) {
+			res = (graph[Vi][ind] == Vj);
+			++ind;
+		}
 	} else {
-		card = graph[V1][0];
-		Vi = V1;
-		Vj = V2;
+		res = true;
 	}
-	
-	ind = 1;
-	while (!res && (ind <= card)) {
-		res = (graph[Vi][ind] == Vj);
-		++ind;
-	}
-	
 	return res;
 }
 
@@ -99,16 +103,18 @@ void generate(int card, int min, int max, char *fileName) {
 	
 	file << card << endl;
 	
-	int cpt, randCard, ind, i, j, Vi, Vj;
+	int ind, i, j, Vi, Vj, cpt;
 	
+	/** The number of edges to add to a covering tree of the graph */
+	int nbEdges = min + ((max-min)/4)*card;
 	/** The table of the unconnected vertices */
-	int *unconnected = new int[card];
+	int *unconnected = new int [card];
 	/** The number of unconnected vertices */
 	int nbUnconnected = card;
 	/** The table of the connected vertices */
-	int *vertices = new int[card];
+	int *vertices = new int [card];
 	/** The number of connected vertices */
-	int nbConnected = 0;
+	int nbVertices = 0;
 	/** The graph
 	 * let i be the vertex considered
 	 * graph[i][0], the number of vertices connected to i
@@ -116,60 +122,132 @@ void generate(int card, int min, int max, char *fileName) {
 	 */
 	int **graph = new int *[card];
 	
-	// We set the indexes of the unconnected vertices (0 to card-1)
+	// We initialize the first table and the graph
 	for (i = 0; i < card; ++i) {
 		unconnected[i] = i;
 		graph[i] = new int[card];
+		graph[i][0] = 0;
 	}
 	
-	// We select the two first vertices to connect
-	Vi = rand() % (card-1);
-	Vj = rand() % card;
-	if (Vi == Vj) {
-		++Vj;
+	// We create a first random covering tree
+	i = rand() % (nbUnconnected-1);
+	j = i + rand() % (nbUnconnected-i);
+	if (i == j) {
+		++j;
 	}
-	
-	// We set the graph
+	Vi = unconnected[i];
+	Vj = unconnected[j];
 	connect(Vi, Vj, graph);
-	
-	// We add them to the connected vertices
+	deleteTwoElements(i, j, &nbUnconnected, unconnected);
 	vertices[0] = Vi;
 	vertices[1] = Vj;
-	nbConnected = 2;
+	nbVertices = nbVertices + 2;
 	
-	// We delete the two elements and shift the table of the unconnected vertices;
-	deleteTwoElements(Vi, Vj, &nbUnconnected, unconnected);
-	
-	// We create a random covering tree
-	while (nbUnconnected > 0) {
-		i = rand() % nbConnected;
-		j = rand() % nbUnconnected;
-		Vi = vertices[i];
-		Vj = unconnected[j];
+	while (nbUnconnected > 1) {
+		i = rand() % nbUnconnected;
+		j = rand() % nbVertices;
+		Vi = unconnected[i];
+		Vj = vertices[j];
 		connect(Vi, Vj, graph);
-		vertices[nbConnected] = Vj;
-		++nbConnected;
-		deleteElement(j, &nbUnconnected, unconnected);
+		deleteElement(i, &nbUnconnected, unconnected);
+		if (graph[Vj][0] >= max) {
+			deleteElement(j, &nbVertices, vertices);
+		}
+		vertices[nbVertices] = Vi;
+		++nbVertices;
 	}
 	
-	// We add randoms connections
-	for (Vi = 0; Vi < card-1; ++Vi) {
-		randCard = min + rand() % (max-min+1);
-		while (graph[Vi][0] < randCard) {
-			Vj = Vi + 1 + (rand() % (card-Vi-1));
+	// If there is one vertex unconnected
+	if (nbUnconnected == 1) {
+		j = rand() % nbVertices;
+		Vi = unconnected[0];
+		Vj = vertices[j];
+		connect(Vi, Vj, graph);
+		deleteElement(0, &nbUnconnected, unconnected);
+		vertices[nbVertices] = Vi;
+		++nbVertices;
+		if (graph[Vj][0] >= max) {
+			deleteElement(j, &nbVertices, vertices);
+		}
+	}
+	
+	// We connect vertices to reach the minimal cardinality on each
+	i = 0;
+	while (i < nbVertices) {
+		Vi = vertices[i];
+		while (graph[Vi][0] < min) {
+			j = rand() % nbVertices;
+			Vj = vertices[j];
 			cpt = 0;
-			while ((Vj < card) && ((areConnected(Vi, Vj, graph)) || (graph[Vj][0] >= max))) {
-				Vj = (Vj+1) % card;
+			while ((cpt < nbVertices) && areConnected(Vi, Vj, graph)) {
+				++cpt;
+				j = (j+1) % nbVertices;
+				Vj = vertices[j];
 			}
+			if (cpt == nbVertices) {
+				deleteElement(i, &nbVertices, vertices);
+				Vi = vertices[i];
+			} else {
+				connect(Vi, Vj, graph);
+				--nbEdges;
+				if (graph[Vj][0] >= max) {
+					deleteElement(j, &nbVertices, vertices);
+					if (i > j) {
+						--i;
+					}
+				}
+			}
+		}
+		++i;
+	}
+		
+	// We add random vertices
+	while ((nbVertices > 1) && (nbEdges > 0)) {
+		i = rand() % nbVertices;
+		j = rand() % nbVertices;
+		Vi = vertices[i];
+		Vj = vertices[j];
+		cpt = 0;
+		while (areConnected(Vi, Vj, graph) && (cpt < nbVertices)) {
+			++cpt;
+			j = (j+1) % nbVertices;
+			Vj = vertices[j];
+		}
+		if (cpt == nbVertices) {
+			deleteElement(i, &nbVertices, vertices);
+		} else {
 			connect(Vi, Vj, graph);
+			--nbEdges;
+			if (graph[Vi][0] >= max) {
+				deleteElement(i, &nbVertices, vertices);
+			}
+			if (graph[Vj][0] >= max) {
+				deleteElement(j, &nbVertices, vertices);
+			}
+			
+			if (graph[Vi][0] > max) {
+				cout << "Vi=" << Vi << endl;
+				cout << "COCO" << endl;
+			}
+			if (graph[Vj][0] > max) {
+				//~ cout << "COCO" << endl;
+			}
 		}
 	}
 	
 	int moy = 0;
+	int minCard = max;
+	int maxCard = 0;
 	// We write in the file
 	for (i = 0; i < card; ++i) {
 		file << i << " ";
 		moy = moy + graph[i][0];
+		if (graph[i][0] < minCard) {
+			minCard = graph[i][0];
+		}
+		if (graph[i][0] > maxCard) {
+			maxCard = graph[i][0];
+		}
 		for (j = 0; j <= graph[i][0]; ++j) {
 			file << graph[i][j] << " ";
 		}
@@ -177,6 +255,8 @@ void generate(int card, int min, int max, char *fileName) {
 	}
 	
 	cout << "moy=" << (double)moy/(double)card << endl;
+	cout << "min=" << minCard << endl;
+	cout << "max=" << maxCard << endl;
 	file.close();
 }
 
